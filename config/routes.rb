4e4e4167578f00578d087edbc3523511a01a668f
MenendezON <menendezon@gmail.com>
2023-11-06 13:36:16 +0000

    Modify the structure of my routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  # With this, you have shorter code to write and you keep it simple, stupid.
+  
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
+  # root "posts#index"
 end
